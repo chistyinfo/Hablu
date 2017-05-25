e4b34483cea454a70425a0d83775c9515f18a285
chisty <chistyinfo@gmail.com>
2017-05-25 23:43:16 +0600

    dialog has added

--- app/src/main/java/greendustbd/hablu/HabluActivity.java
+++ app/src/main/java/greendustbd/hablu/HabluActivity.java
@@ -1,13 +1,45 @@
 package greendustbd.hablu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.andreilisun.swipedismissdialog.SwipeDismissDialog;
 
 public class HabluActivity extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hablu);
+        button1= (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showAddUserDialog();
+            }
+        });
+    }
+
+    private void showAddUserDialog() {
+        View dialog = LayoutInflater.from(this).inflate(R.layout.dialog_add_user, null);
+        final SwipeDismissDialog swipeDismissDialog = new SwipeDismissDialog.Builder(this)
+                .setView(dialog)
+                .build()
+                .show();
+
+
+    }
+    private void showAlertDialog() {
+        new SwipeDismissDialog.Builder(this)
+                .setLayoutResId(R.layout.dialog_success_booking)
+                .build()
+                .show();
+
     }
 }
